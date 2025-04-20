10c8371196e271e0d48decbf81318e63e64105c1
Justin Sjaaheim <justinsjaaheim@icloud.com>
2025-04-19 20:13:16 -0500

    Initial commit for new version — Avalonia UI Grid 1 — Dark Theme

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace CreativeFileBrowser.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Title { get; set;} = "Creative File Browser";
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace CreativeFileBrowser.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
