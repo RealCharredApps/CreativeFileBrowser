b8e0d406121c03cb1d843adfdec97fe9cd1b6ff0
Justin Sjaaheim <justinsjaaheim@icloud.com>
2025-04-22 16:25:19 -0500

    Experiment with UI view like ms Teams — Easy to understand and integrate - left to center - needs center and main views/dynamic content after

--- obj/Debug/net9.0/CreativeFileBrowser.AssemblyInfo.cs
+++ obj/Debug/net9.0/CreativeFileBrowser.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CreativeFileBrowser")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67eeacc662f15e6304f057d34c5acab846b4b6af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45e639a9f92a9bca54df382341d623335dc47ed3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CreativeFileBrowser")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CreativeFileBrowser")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
